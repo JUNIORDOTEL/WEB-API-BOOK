179c04fdee7ae30ef1e3dd869b115a5693d31e6e
DOTEL RAMIREZ, EDWARD JUNIOR <20160055@unapec.edu.do>
2021-09-01 12:10:09 -0400

    WEB API BOOKS .NET

---
+++ WEB API/Controllers/BookController.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WEB_API.Models;
+
+namespace WEB_API.Controllers
+{
+    public class BookController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            string query = @"
+                select ID,Title,Description,PageCount,Excerpt,PublishDate,Test from dbo.Book";
+
+            DataTable table = new DataTable();
+            using (var conn = new SqlConnection(ConfigurationManager.
+                ConnectionStrings["BookDB"].ConnectionString))
+            using (var cmd = new SqlCommand(query, conn))
+            using (var da = new SqlDataAdapter(cmd))
+            {
+                cmd.CommandType = CommandType.Text;
+                da.Fill(table);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, table);
+            
+
+
+
+        }
+
+        public string Post(Book dep)
+        {
+
+            try
+            {
+                string query = @"
+                    insert into dbo.Book values
+                     ('" + dep.Test + @"')
+                      ";
+
+                DataTable table = new DataTable();
+                using (var conn = new SqlConnection(ConfigurationManager.
+                    ConnectionStrings["BookDB"].ConnectionString))
+                using (var cmd = new SqlCommand(query, conn))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+
+                return "Agregado correctamente";
+            }
+            catch (Exception)
+            {
+
+                return "Falla al agregar";
+            }
+        }
+
+
+        public string Put(Book dep)
+        {
+
+            try
+            {
+                string query = @"
+                    update dbo.Book set Title = 
+                     '" + dep.Title + @"'
+                      where ID = "+ dep.ID+@"
+                      ";
+
+                DataTable table = new DataTable();
+                using (var conn = new SqlConnection(ConfigurationManager.
+                    ConnectionStrings["BookDB"].ConnectionString))
+                using (var cmd = new SqlCommand(query, conn))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+
+                return "Editado correctamente";
+            }
+            catch (Exception)
+            {
+
+                return "Falla al editar";
+            }
+        }
+
+
+        public string Delete(int id)
+        {
+
+            try
+            {
+                string query = @"
+                    Delete from dbo.Book
+                      where ID = " + id + @"
+                      ";
+
+                DataTable table = new DataTable();
+                using (var conn = new SqlConnection(ConfigurationManager.
+                    ConnectionStrings["BookDB"].ConnectionString))
+                using (var cmd = new SqlCommand(query, conn))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+
+                return "Eliminado correctamente";
+            }
+            catch (Exception)
+            {
+
+                return "Falla al Eliminar";
+            }
+        }
+
+
+    }
+}
---
+++ WEB API/Models/Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WEB_API.Models
+{
+    public class Book
+    {
+        public int ID { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string PageCount { get; set; }
+
+        public string Excerpt { get; set; }
+
+        public string PublishDate { get; set; }
+
+        public string Test { get; set; }
+    }
+}
